745d7dd8aa0eae7a6a04998882aa3f1a93f3614b
Pedro <pcordista368@gmail.com>
2020-01-29 19:36:22 -0300

    FrontEnd

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col s12 m6 l6">
+            <div class="col s12 m6 l4">
                 <a href="#">
                     <img src="assets/img/silhouette.png" alt="" class="footer-logo">
                 </a>
                 <div class="margin_2_top row">
-                    <div class="col s6 m3 footer-links textUpper">
+                    <div class="col s6 m6 footer-links textUpper">
                         <div>
                             <a href="#">
                                 antes e depois
@@ -23,7 +23,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col s6 m3 footer-links textUpper">
+                    <div class="col s6 m6 footer-links textUpper">
                         <div>
                             <a href="#">
                                 depoimentos
@@ -40,32 +40,28 @@
                             </a>
                         </div>
                     </div>
-                    <div class="col s12 m6 footer-follow margin_1_top_mobile">
-                        <span class="white-text">
-                            Acompanhe as novidades em nossas redes sociais
-                        </span>
-                        <div class="margin_1_top">
-                            <a href="#">
-                                <i class="fab fa-facebook"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
-            <div class="col s12 m3 l3 footer-news margin_2_top_mobile">
-                <strong>Subscript to Sinclair Pharma via Email</strong>
-                <p class="white-text">
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                </p>
-                <div class="footer-inputs flex">
-                    <input type="text" placeholder="Email Address">
-                    <button class="white black-text">subscribe</button>
+            <div class="col s12 m3 l4 footer-news margin_2_top_mobile">
+                <div class="footer-newsletter white-text">
+                    Acompanhe as novidades em nossas redes sociais
+                </div>
+                <div class="margin_1_top footer-redes">
+                    <a href="#" target="_blank">
+                        <i class="fab fa-facebook white-text"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-instagram white-text"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-linkedin white-text"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-youtube white-text"></i>
+                    </a>
                 </div>
             </div>
-            <div class="col s12 m3 l3 footer-sinclair center-align margin_1_top_mobile">
+            <div class="col s12 m3 l4 footer-sinclair center-align margin_1_top_mobile">
                 <a href="#">
                     <img src="assets/img/sinclairpharma.png" alt="">
                 </a>
@@ -74,4 +70,21 @@
             </div>
         </div>
     </div>
+    
 </footer>
+
+<div class="copyright-container white-text">
+        <div class="copyright">
+            <div class="copyright-footer center-align">
+                <div class="footer-text">
+                    <h6>©copyright | Sinclair Pharma. Todos os direitos reservados.</h6>
+                </div>
+            </div>
+            <div class="desenvolvido development" style="vertical-align: middle;">
+                <a style="color: white!important;" href="http://www.crearepp.com.br/" target="_blank" rel="noopener">
+                    Desenvolvido por:
+                    <img class="img-fluid" src="assets/img/crearepp.png" alt="Creare Publicidade e Propaganda">
+                </a>
+            </div>
+        </div>
+    </div>
